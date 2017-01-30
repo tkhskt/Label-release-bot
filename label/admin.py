18621ac0d7f25526ed5ba49708483a247804f79d
Keita <likenirvana@me.com>
2017-01-30 21:12:00 +0900

    retry

--- label/admin.py
+++ label/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from label.models import sensedb,warpdb,altemadb,bunkaidb,maltinedb,flaudb,progressivedb,trekkiedb,planetdb,lineid
+from label.models import sensedb,warpdb,altemadb,bunkaidb,maltinedb,flaudb,progressivedb,trekkiedb,planetdb,lineid,diggerdb
 
 admin.site.register(sensedb)
 admin.site.register(warpdb)
@@ -14,3 +14,4 @@ admin.site.register(progressivedb)
 admin.site.register(trekkiedb)
 admin.site.register(planetdb)
 admin.site.register(lineid)
+admin.site.register(diggerdb)
