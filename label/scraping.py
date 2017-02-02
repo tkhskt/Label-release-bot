097aa2eba11ea7c25b0cf9bfb26c8676316310f5
Keita <likenirvana@me.com>
2017-02-03 08:59:16 +0900

    retry

--- label/scraping.py
+++ label/scraping.py
@@ -791,15 +791,15 @@ class never:
             neverdb.append(artdb.artist)
 
 
-        for tit in soup.find_all("p",class_="title"):
-          title.append(tit.text)
+        #for tit in soup.find_all("p",class_="title"):
+         # title.append(tit.text)
 
 
-        for i in range(len(title)):
-           title[i] = title[i].replace("\n","")
+       # for i in range(len(title)):
+        #   title[i] = title[i].replace("\n","")
 
-        for i in range(len(title)):
-           title[i] = re.sub("\s{2}","",title[i])
+        #for i in range(len(title)):
+         #  title[i] = re.sub("\s{2}","",title[i])
 
         for link in soup.find_all("div",class_="leftMiddleColumns"):
           for link2 in link.find_all("a"):
@@ -809,7 +809,7 @@ class never:
             url[i] = 'https://never-slept.bandcamp.com' + url[i]
 
 
-        if len(title)>len(artistdb):
+        if len(url)>len(artistdb):
             #info['title']=title[0]
             #info['artist']=artist[0]
             info['url']=url[0]
