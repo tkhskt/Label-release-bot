73c0a59cd6d4c35bd8119983b97d2d745f5db104
Keita <likenirvana@me.com>
2017-02-03 09:32:55 +0900

    retry

--- label/scraping.py
+++ label/scraping.py
@@ -791,16 +791,6 @@ class never:
             neverdb.append(artdb.artist)
 
 
-        #for tit in soup.find_all("p",class_="title"):
-         # title.append(tit.text)
-
-
-       # for i in range(len(title)):
-        #   title[i] = title[i].replace("\n","")
-
-        #for i in range(len(title)):
-         #  title[i] = re.sub("\s{2}","",title[i])
-
         for link in soup.find_all("div",class_="leftMiddleColumns"):
           for link2 in link.find_all("a"):
              url.append(link2['href'])
