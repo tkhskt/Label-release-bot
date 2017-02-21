00c2eb3cd7438d4ad6af7a458e641a92c8c77b22
Keita <likenirvana@me.com>
2017-02-21 18:57:36 +0900

    re

--- linebot/wsgi.py
+++ linebot/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linebot.settings")
 
+#application = get_wsgi_application()
+
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
