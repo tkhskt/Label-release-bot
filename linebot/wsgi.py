6193a1b6436bb0a17e8d18e81c221e082ed96e3c
Keita <likenirvana@me.com>
2017-02-22 15:00:54 +0900

    retry

--- linebot/wsgi.py
+++ linebot/wsgi.py
@@ -13,10 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linebot.settings")
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
