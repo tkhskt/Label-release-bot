584c1f46ab48b9602a64baf72e4ce2fdae87989c
Keita <likenirvana@me.com>
2017-02-26 01:06:12 +0900

    anticon

--- label/scraping.py
+++ label/scraping.py
@@ -1604,10 +1604,16 @@ class scrape:
 
 
         if url[0]!=artistdb[0]:
-            info['title']=title[0]
-            info['artist']=artist[0]
-            info['url']=url[0]
-            info['key']=1
+            if url[1]!=artistdb[0]:
+                info['title']=title[0]
+                info['artist']=artist[0]
+                info['url']=url[0] + "\n複数のリリースがあります"
+                info['key']=1
+            else:
+                info['title']=title[0]
+                info['artist']=artist[0]
+                info['url']=url[0]
+                info['key']=1
 
         delete = releases.objects.filter(label='anticon')
         delete.delete()
