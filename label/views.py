12e8e3f35b87e027180e4338f918247ae1f6e20f
Keita <bonjovi1926@yahoo.co.jp>
2017-06-17 18:48:55 +0900

    ne

--- label/views.py
+++ label/views.py
@@ -111,18 +111,18 @@ def push(text,token):
 
 
 def labelcheck(request,page):
-     #res = 'OK' + page
-     #er = 'error' + page
-     #try:
+     res = 'OK' + page
+     er = 'error' + page
+     try:
        for lb in labelname[int(page)]:
          info = scrape().doscraping(lb)
          if info['key']==1:
            linetransmit(info['label'],info['title'],info['artist'],info['url'])
            db = update(label=info['label'],url=info['url'].replace('\n複数のリリースがあります',''))
            db.save()
-      # return HttpResponse(res)
-     #except:
-      # return HttpResponse(er)
+       return HttpResponse(res)
+     except:
+       return HttpResponse(er)
 
 
 
