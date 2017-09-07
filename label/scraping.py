9b8d64dedb9df1a777f8d7f3e1dff590bedba645
Keita <bonjovi1926@yahoo.co.jp>
2017-09-08 02:55:17 +0900

    fix: flau

--- label/scraping.py
+++ label/scraping.py
@@ -512,19 +512,14 @@ class scrape:
             artistdb.append(artdb.url)
 
 
-        for tit in soup.find_all("section",class_="unit"):
-          for h3 in tit.find_all("h3"):
-             title.append(h3.text)
-          for p in tit.find_all("p"):
-             if p.text!='\xa0' and p.text!=' ' and p.text!='':
-                   artist.append(p.text)
-
-          for link in tit.find_all("a",href=True):
-             if link['href'] != '':
-                if  'http://' in link['href']:
-                       url.append(link['href'])
-                else:
-                       url.append('http://flau.jp/' + link['href'])
+        for glid in soup.find_all(class_="glid_box spnone"):
+            txt = []
+            for a in glid.find_all("a"):
+                url.append(a['href'])
+            for p in glid.find_all(class_="archive-releases_title"):
+                txt.append(p.text)
+            artist.append(txt[0])
+            title.append(txt[1])
 
         if len(url)>len(artistdb):
             if url[1]!=artistdb[0]:
