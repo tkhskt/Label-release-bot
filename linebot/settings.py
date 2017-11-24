40df42cba615305b293e6a4aa97ca6cf85de003f
Keita <bonjovi1926@yahoo.co.jp>
2017-11-24 13:13:46 +0900

    travis db setting

--- linebot/settings.py
+++ linebot/settings.py
@@ -76,28 +76,28 @@ WSGI_APPLICATION = 'linebot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'relbot',
-        'USER':     'root',
-        'PASSWORD': '',
-        'HOST':     '127.0.0.1',
-        'PORT':     '',
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.mysql',
+            'NAME':     'relbot',
+            'USER':     'root',
+            'PASSWORD': '',
+            'HOST':     '127.0.0.1',
+            'PORT':     '',
+        }
     }
-}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'relbot',
-#        'HOST': 'localhost',
-#        'PORT': '3306',
-#        'USER': 'root',
-#        'PASSWORD': 'Keita9156'
-#    }
-#}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'relbot',
+            'HOST': 'localhost',
+            'PORT': '3306',
+            'USER': 'root',
+            'PASSWORD': 'Keita9156'
+        }
+    }
 
 
 # Password validation
